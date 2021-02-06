0628cc69cb4934c89200c4fb5851e8649dea32f6
Jo-R <jo.rigg8@gmail.com>
2021-02-06 16:02:02 +0000

    add some styles to the detail page

--- src/features/runActivities/ViewRunActivity.jsx
+++ src/features/runActivities/ViewRunActivity.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectActivityById } from "./runActivitiesSlice";
+import { Card } from "../../components/Card/Card";
+import typeStyles from "../../sharedStyles/Typography.module.css";
 
+// TODO mock map image and other details
 export const ViewRunActivity = ({ match }) => {
   const runId = match.params.runId;
 
   const activity = useSelector((state) => selectActivityById(state, runId));
 
   return (
-    <div>
+    <Card>
       {activity ? (
         <>
-          <p>{activity.title}</p>
+          <h2 className={typeStyles.cardHeading}>{activity.title}</h2>
           <p>{`distance ${activity.distanceMile}`}</p>
         </>
       ) : (
         <p>No such activity</p>
       )}
-    </div>
+    </Card>
   );
 };
--- src/layout/ActivityDetailPage.jsx
+++ src/layout/ActivityDetailPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ViewRunActivity } from "../features/runActivities/ViewRunActivity";
+import { AddRunActivity } from "../features/runActivities/AddRunActivity";
 import { PageLayout } from "./PageLayout";
 
 export const ActivityDetailPage = ({ children, ...props }) => {
   return (
     <PageLayout>
-      <>
-        <ViewRunActivity {...props} />
-      </>
+      <ViewRunActivity {...props} />
+      <AddRunActivity {...props} />
     </PageLayout>
   );
 };
